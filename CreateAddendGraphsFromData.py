810d6ebcee1e172044847207a51a4887d609c3ca
Oleg Sheydvasser <oleg.sheydvasser@autodesk.com>
2022-03-06 19:13:16 -0500

    add title to the 2nd graph

--- CreateAddendGraphsFromData.py
+++ CreateAddendGraphsFromData.py
@@ -13,7 +13,7 @@ figure, axes = plt.subplots(2)
 Lambda = 2.44344296778474
 
 # determine the data folder
-file_index = 7
+file_index = 8
 file_suffix = FileNames.get_file_name_suffix(file_index)
 data_folder = "Data-" + file_suffix
 print(data_folder)
@@ -53,6 +53,7 @@ with open(table_file_name) as table_file:
 
 
 # process each growing sequence
+axes[0].set_title("Growing addend sequences")
 for data_file_name in data_files:
     # Second, extract the addend sequences for the given ulam seq length.
     # read the file
@@ -62,9 +63,9 @@ for data_file_name in data_files:
             addend_seq.append(line) #TODO do in one line
     # Determine parameters for graphing
     x = range(len(addend_seq))
-    axes[0].plot(x,addend_seq) #TODO fix ticks for y-axis
-    axes[0].set_title("Growing addend sequences")
+    axes[0].plot(x,addend_seq) 
 
+axes[1].set_title("Addend residues")
 axes[1].hist(residues_growing_addends, bins = 60, label = 'Growing addends residue')
 axes[1].hist(residues_flat_addends, bins = 60, color = 'red', label = 'Flat addends residue')
 axes[1].set_xticks([0,1/3,2/3,1])
@@ -72,7 +73,7 @@ axes[1].legend()
 
 axes[0].grid(axis='x')
 axes[0].set_yticks([0, len(addend_seq)/9, 2*len(addend_seq)/9, len(addend_seq)/3])
-plt.savefig(f'{data_folder}/Growing addend sequences.png')
+plt.savefig(f'{data_folder}/Addend sequences and residues.png')
 plt.show()
 plt.close()
 
